0b21b49c7c1d5cbc4e53f0b58837cf17b0abbff4
marcobt15 <mtoito03@gmail.com>
2023-12-09 17:19:41 -0500

    pieces fit kinda

--- pieces/piece.py
+++ pieces/piece.py
@@ -2,7 +2,7 @@ class Piece():
     def __init__(self, game, row, col, colour):
         self.row = row
         self.col = col
-        self.image = game.image.load("images/"+colour+".png")
+        self.image = game.transform.scale(game.image.load("images/"+colour+".png"), (100, 100))
     
     def draw(self, game, screen, size):
         screen.blit(self.image, game.Rect(self.col*size, self.row*size, size, size))
